d4cc6e578e9ec058019b2a9f6e095cb45c4ac79b
benkaan001 <benkaan001@gmail.com>
2023-02-28 23:56:53 -0600

    add extract_count

--- analyze_data/ted_talks.py
+++ analyze_data/ted_talks.py
@@ -21,3 +21,26 @@ def str_to_list(ratings_str: str) -> list:
         return ast.literal_eval(ratings_str)
     except (ValueError, SyntaxError):
         raise ValueError("Invalid string representation of a Python list object.")
+
+
+def extract_count(dict_list):
+    """
+    Extracts the count values from a list of dictionaries and returns their sum.
+
+    Parameters:
+    -----------
+    dict_list: list of dict
+        A list of dictionaries containing 'count' values.
+
+    Returns:
+    --------
+    int
+        The sum of the 'count' values in the input list of dictionaries.
+
+    Example:
+    --------
+    >>> dict_list = [{'count': 3}, {'count': 5}, {'count': 2}]
+    >>> extract_count(dict_list)
+    10
+    """
+    return sum(int(d['count']) for d in dict_list)
